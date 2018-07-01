767c67cffedda201dd46c9762c7ecc4737129108
kingdomCome <40040615+kingdomcome82@users.noreply.github.com>
2018-07-01 21:28:10 +0100

    functionally completed files updated

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayE() {
+  var decimalPlaces = document.querySelector('input');
+  var displayParagraph = document.querySelector('p');
+
+if (decimalPlaces.value < 0 | decimalPlaces.value > 40 | decimalPlaces.value === "") {
+  displayParagraph.innerHTML = "Please enter a number between 0 and 40";
+} else
+  displayParagraph.innerHTML = Number(Math.E).toFixed(decimalPlaces.value); }
